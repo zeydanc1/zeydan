da14d282a7e5c14e90b8076d49787e303f2e076b
zeydanc1 <https://github.com/zeydanc1/zeydan>
2026-03-31 00:43:23 +0300

    ilk commit

--- Project/src/model/EpisodeStatus.java
+++
@@ -1,8 +0,0 @@
-package model;
-// Defines that this enum belongs to the "model" package
-
-public enum EpisodeStatus {
-    DRAFT,
-    SCHEDULED,
-    PUBLISHED
-}
--- Project/src/model/ScheduleConflictException.java
+++
@@ -1,9 +0,0 @@
-package model;
-// Defines that this class belongs to the "model" package
-
-public class ScheduleConflictException extends Exception {
-
-    public ScheduleConflictException(String msg) {
-        super(msg);
-    }
-}
